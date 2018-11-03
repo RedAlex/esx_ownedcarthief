df2a6e202209c2662d649875414924e80905c540
Alex Garcio <855331+RedAlex@users.noreply.github.com>
2018-11-03 19:52:13 -0400

    Fix description

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config                      = {}
-Config.AlertPolice          = true --Turn to false if you don't want cops is called on car alarm start for player car
-Config.BlipTime             = 40   --Set how many time a blip keep flash on map
-Config.SuccesChance         = 80%  --How many % of chance for the car alarm start AND call cops.
-Config.PoliceNumberRequired = 0    --Set how many cops is need to try stole a car
+Config.AlertPolice          = true --Turn to false if you don't want cops is called on car alarm start for player car.
+Config.BlipTime             = 40   --Set how many time a blip keep flash on map.
+Config.SuccesChance         = 80%  --How many % of chance to unlock a car.
+Config.PoliceNumberRequired = 0    --Set how many cops is need to try stole a car.
 Config.CallCopsChance       = 80%  --How many % of chance for the car alarm start AND call cops.
 Config.Locale               = 'fr'
